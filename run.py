153265c519be29e49e7202620742c0802f99c55a
dgdcosgrove <dgdcosgrove@gmail.com>
2022-12-17 02:41:09 +0000

    Add welcome statement and build board for gameplay

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+#Import functionality to generate random integers
+from random import randint
+
+def make_board():
+    """
+    Create board to contain game
+    """
+    board = []
+    
+    for x in range(0, 5):
+        board.append(["0"] * 5)
+    
+    for row in board:
+        print("---".join(row))
+
+print("Welcome to Battleships!")
+print("Please choose a number between 0 and 4")
+print("Legend: Top left is row 0 and column 0")
+make_board()
+
+
