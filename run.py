14207ccd9643ce1387c36cf3588ea1e25c04a8e8
dgdcosgrove <dgdcosgrove@gmail.com>
2022-12-17 06:36:58 +0000

    Add function to check user guess vs ship position and return result

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 # Import functionality to generate random integers
 from random import randint
 
-
 def make_board():
     """
     Create board to contain game
@@ -33,14 +32,33 @@ def guess_position():
     guess_col = int(input("Guess Column: "))
     return [guess_row, guess_col]
 
+def check_guess(ship_location, user_guess):
+    """
+    Function to check user guess against ship position
+    """
+    row, col = user_guess
 
+    if ship_location == user_guess:
+        print("You have guessed correctly, the battleship has been sunk!")
+    elif row not in range(5) or col not in range(5):
+        print("Please select a number between 0-4..")
+    elif board[row][col] == "X":
+        print("Duplicate selection, please make unique selection..")
+    else:
+        print("Your guess was incorrect!")
+        board[row][col] = "X"
+        for row in board:
+            print(" --- ".join(row))
 
-print("Welcome to Battleships!\n")
-print("Please choose a number between 0 and 4")
-print("Legend: Top left is row 0 and column 0\n")
-make_board()
-print("\n")
-ship_location = ship_position(board)
-print(ship_location)
-user_guess = guess_position()
-print(user_guess)
+def main():
+    print("Welcome to Battleships!\n")
+    print("Please choose a number between 0 and 4")
+    print("Legend: Top left is row 0 and column 0\n")
+    make_board()
+    ship_location = ship_position(board)
+    print("\n")
+    user_guess = guess_position()
+    print("\n")
+    check_guess(ship_location, user_guess)
+
+main()
